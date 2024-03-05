f3c71dfbc4d407698d6d25e61712a6697ff0a208
Mikers <snissn@gmail.com>
2024-03-04 20:12:46 -1000

    fluence ipc subnet

--- packages/app/src/utils/network.ts
+++ packages/app/src/utils/network.ts
@@ -1,23 +1,27 @@
 import { defineChain } from 'viem'
 
 const fluence = defineChain({
+  blockExplorers: { default : {name: "TBD", url: "https://place.holder.com"} } , // TODO
   rpcUrls: {
     default: {
-      http: ['http://127.0.0.1:8745'],
+      http: ['http://localhost:8010/proxy'] // need cors proxy
+      // lcp --proxyUrl http://localhost:8745
     },
   },
+  testnet: true,
   name: 'Fluence',
   nativeCurrency: {
     decimals: 18,
     name: 'Fluence',
     symbol: 'FLT',
   },
-  id: '0x482a2b14a7af',
+  id: 2443544213400835,
 })
 
 import { filecoinCalibration, polygonMumbai, localhost, Chain } from 'viem/chains'
 
 let chains = [filecoinCalibration, polygonMumbai, fluence] as [Chain, ...Chain[]]
+console.log('Chains', chains)
 
 export const ETH_CHAINS = chains
 
