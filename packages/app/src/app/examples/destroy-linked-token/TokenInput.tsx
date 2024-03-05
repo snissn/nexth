dfc45904091c0c562a8bacbfb9a647dd1ae634c1
Mikers <snissn@gmail.com>
2024-03-04 20:12:55 -1000

    destroy linked token

---
+++ packages/app/src/app/examples/destroy-linked-token/TokenInput.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { isValidAddress } from '../utils/address'
+import { Address } from '../types'
+
+type TokenInputProps = {
+  setTokenAddress: (address: Address) => void
+  tokenAddress: Address
+}
+
+const TokenInput: React.FC<TokenInputProps> = ({ setTokenAddress, tokenAddress }) => {
+  return (
+    <label className='form-control w-full mt-10'>
+      <div className='label'>
+        <span className='label-text'>ERC-20 Token address</span>
+      </div>
+      <div className='label'>
+        <span className='label-text'>{tokenAddress}</span>
+      </div>
+    </label>
+  )
+}
+
+export default TokenInput
